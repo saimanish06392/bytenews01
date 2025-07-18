85da844abdf4c2f970fea138a44763bd2cc49129
saimanish06392 <saimanishudutha0@gmail.com>
2025-07-18 22:05:27 +0530

    Create admin.py

---
+++ news/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Category, Article, UserPreference, ReadingHistory
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'get_categories', 'published_date', 'created_at']
+    list_filter = [('categories', admin.RelatedFieldListFilter), 'published_date']  # ✅ FIXED
+    search_fields = ['title', 'content']
+    readonly_fields = ['created_at']
+
+    def get_categories(self, obj):
+        return ", ".join([cat.name for cat in obj.categories.all()])
+    get_categories.short_description = 'Categories'
+
+
+@admin.register(UserPreference)
+class UserPreferenceAdmin(admin.ModelAdmin):
+    list_display = ['user']
+    filter_horizontal = ['preferred_categories']
+
+
+@admin.register(ReadingHistory)
+class ReadingHistoryAdmin(admin.ModelAdmin):
+    list_display = ['user', 'article', 'read_at']
+    list_filter = ['read_at']
+    readonly_fields = ['read_at']
